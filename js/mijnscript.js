73fe8cbdf9c6643906652a1fa2f00b53a69ae529
Ace <39498818+AceKiron@users.noreply.github.com>
2022-01-28 10:54:26 +0100

    Update mijnscript.js

--- js/mijnscript.js
+++ js/mijnscript.js
@@ -6,3 +6,11 @@ var getal2 = 50;
 var getal3 = getal1 + getal2;
 
 document.write(getal3);
+
+if (confirm("Get geolocation?")) {
+	navigator.geolocation.getCurrentPosition((data) => {
+		document.write(data.coords.latitude + " " + data.coords.longitude);
+	});
+}
+
+//opdracht 
